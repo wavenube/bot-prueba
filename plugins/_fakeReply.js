f93c3ea2fc1eb1f0ee89dfb0d546dd44f3c736d3
wavenube <wavenub3@gmail.com>
2024-08-11 11:53:54 +0100

    Update _fakeReply.js

--- plugins/_fakeReply.js
+++ plugins/_fakeReply.js
@@ -1,76 +1,128 @@
-import fetch from 'node-fetch'
 
-let handler = m => m
-handler.all = async function (m) {
-	
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? this.user.jid : m.sender
-let pp = await this.profilePictureUrl(who, 'image').catch(_ => "https://i.ibb.co/Lnmr4HN/abyss5.png")
-	
-global.rpl = { contextInfo: { externalAdReply: { mediaUrl: nna, mediaType: 'VIDEO', description: 'Actualización/novedades', title: packname, body: 'Canal update', thumbnailUrl: pp, sourceUrl: nna }}} 
-   	
-global.fake = { contextInfo: { mentionedJid: null, forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363318622514917@newsletter', serverMessageId: '', newsletterName: 'Abyss┃ᴮᴼᵀ' }, externalAdReply: { title: wm, body: vs, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: pp, sourceUrl: redes.getRandom() }}}
-   
-global.rpyp = { contextInfo: { externalAdReply: { mediaUrl: md, mediaType: 'VIDEO', description: 'GitHub', title: 'GitHub', body: 'ˢᶦ ᵗᵉ ᵃᵍʳᵃᵈᵃ ᵉˡ ᴮᵒᵗ ᵃᵖᵒʸᵃʳᵐᵉ ᶜᵒⁿ ᵘⁿᵃ 🌟', thumbnailUrl: pp, sourceUrl: md }}}
+import fetch from 'node-fetch';
+export async function before(m, { conn }) {
+   let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/Lnmr4HN/abyss5.png');
 
-//⊱ ━━━━━.⋅ RPG ⋅.━━━━ ⊰
+  let nam = "✨  Abyss - Bot  ✨"
+  
+  // Respuesta con enlace de WhatsApp
+  global.rpl = {
+    contextInfo: {
+    	isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: id_canal,
+      serverMessageId: 100,
+      newsletterName: nam,
+    }, 
+      externalAdReply: {
+        mediaUrl: bgp,
+        mediaType: 'VIDEO',
+        description: 'support group',
+        title: packname,
+        body: 'grupo de soporte',
+        thumbnailUrl: pp,
+        sourceUrl: bgp
+      }
+    }
+  };
+  
+  // Respuesta con enlace de Canal de WhatsApp
+  global.rcanal = {
+    contextInfo: {
+    	isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: id_canal,
+      serverMessageId: 100,
+      newsletterName: nam,
+    }, 
+      externalAdReply: {
+        mediaUrl: canal,
+        mediaType: 'VIDEO',
+        description: 'canal del grupo',
+        title: packname,
+        body: 'Canal de Abyss',
+        thumbnailUrl: pp,
+        sourceUrl: canal
+      }
+    }
+  }
+  
+    // Forwarded
+  global.fwc = {
+    contextInfo: {
+    	isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: id_canal,
+      serverMessageId: 100,
+      newsletterName: nam,
+    }
+    }
+  }
 
-global.flaaa = [
-'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text=',
-'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&text=',
-'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=',
-'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
-'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
+  // Respuesta con enlace de PayPal
+  global.rpyp = {
+    contextInfo: {
+    	isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: id_canal,
+      serverMessageId: 100,
+      newsletterName: nam,
+    }, 
+      externalAdReply: {
+        mediaUrl: pyp,
+        mediaType: 'VIDEO',
+        description: 'Donate',
+        title: 'PayPal',
+        body: 'ayuda a mantener el bot activo',
+        thumbnailUrl: pp,
+        sourceUrl: pyp
+      }
+    }
+  };
 
-global.cmenut = '❖––––––『'
-global.cmenub = '┊✦ '
-global.cmenuf = '╰━═┅═━––––––๑\n'
-global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     '
-global.dmenut = '*❖─┅──┅〈*'
-global.dmenub = '*┊»*'
-global.dmenub2 = '*┊*'
-global.dmenuf = '*╰┅────────┅✦*'
-global.htjava = '⫹⫺'
-global.htki = '*⭑•̩̩͙⊱•••• ☪*'
-global.htka = '*☪ ••••̩̩͙⊰•⭑*'
-global.comienzo = '• • ◕◕════'
-global.fin = '════◕◕ • •'
-global.botdate = `⫹⫺ Date :  ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}` //Asia/Jakarta
-global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}`//America/Los_Angeles
-global.fgif = {
-key: {
-participant : '0@s.whatsapp.net'},
-message: { 
-"videoMessage": { 
-"title": wm,
-"h": `Hmm`,
-'seconds': '999999999', 
-'gifPlayback': 'true', 
-'caption': bottime,
-'jpegThumbnail': fs.readFileSync('./media/abyss.png')
-}}}
+  // Respuesta con enlace de Instagram
+  global.rpig = {
+    contextInfo: {
+    	isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: id_canal,
+      serverMessageId: 100,
+      newsletterName: nam,
+    }, 
+      externalAdReply: {
+        mediaUrl: ig,
+        mediaType: 'VIDEO',
+        description: 'Sigueme por Instagram',
+        title: 'Instagram',
+        body: 'sigueme por Instagram',
+        thumbnailUrl: pp,
+        sourceUrl: ig
+      }
+    }
+  };
 
-/*----------------------[ SALIDA DEL GRUPO ]-----------------------*/
-if (!m.isGroup) return
-let chats = global.db.data.chats[m.chat]
-if (!chats.expired) return !0
-if (+new Date() > chats.expired) {
-await this.reply(m.chat, [`*${this.user.name}* ᴹᵉ ᵛᵒʸ ᵈᵉˡ ᵉˡ ᵍʳᵘᵖᵒ ᶠᵘᵉ ᵘⁿ ᵍᵘˢᵗᵒ ᵉˢᵗᵃ ᵃᵠᵘᶦ́ ˢᶦ ᵠᵘᶦᵉʳᵉˢ ᵠᵘᵉ ᵛᵘᵉˡᵛᵃ ᵁˢᵉʳ ᵈᵉ ⁿᵘᵉᵛᵒ ᵉˡ ᶜᵒᵐᵃⁿᵈᵒ`, `Bueno me voy de este grupo de mrd, no me agregue a grupo ptm`, `*${this.user.name}* me voy de este grupito culiado nada interesante yo queria ver teta y son puro gays aca 🤣`].getRandom()) 
-await this.groupLeave(m.chat)
-chats.expired = null
+  // Respuesta con enlace de YouTube
+  global.rpyt = {
+    contextInfo: {
+    	isForwarded: true,
+    forwardedNewsletterMessageInfo: {
+      newsletterJid: id_canal,
+      serverMessageId: 100,
+      newsletterName: nam,
+    }, 
+      externalAdReply: {
+        showAdAttribution: true,
+        mediaUrl: yt,
+        mediaType: 'VIDEO',
+        description: 'Suscribete: ' + yt,
+        title: 'YouTube',
+        body: 'aprende a crear tus propios bots',
+        thumbnailUrl: pp,
+        sourceUrl: yt
+      }
+    }
+  }
+  
+  
+  
 }
-
-/*----------------------[ FIN DE PREMIUM ]-----------------------*/
-for (const user of Object.values(global.db.data.users)) {
-if (user.premiumTime != 0 && user.premium) {
-if (new Date() * 1 >= user.premiumTime) {
-user.premiumTime = 0;
-user.premium = false;
-const JID = Object.keys(global.db.data.users).find((key) => global.db.data.users[key] === user);
-const usuarioJid = JID.split`@`[0];
-const textoo = `*⚠️ @${usuarioJid} 𝚃𝚄 𝚃𝙸𝙴𝙼𝙿𝙾 𝙲𝙾𝙼𝙾 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼 𝙷𝙰 𝙴𝚇𝙿𝙸𝚁𝙰𝙳𝙾, 𝚈𝙰 𝙽𝙾 𝙴𝚁𝙴𝚂 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*`;
-await this.sendMessage(JID, {text: textoo, mentions: [JID]}, {quoted: ''});
-}}}}
- 
-export default handler
-
- 
